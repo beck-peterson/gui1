ed2e2fc85582fdfbcb75d835713b4c5d9ef90fc5
beck-peterson <56322362+beck-peterson@users.noreply.github.com>
2019-12-01 14:28:25 -0500

    trying to fix jquery

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ function enableJquery() {
     var script = document.createElement("script");
     script.src = "https://code.jquery.com/jquery-3.4.1.min.js";
     script.type = "text/javascript";
-    document.getElementsByTagName("head")[0].appendChild(script);
+    document.getElementsByTagName("script")[0].prepend(script);
 }
 enableJquery();
 
