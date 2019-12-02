4f01c871db907b7d4468a70f48f83d17b4a98a4a
beck-peterson <56322362+beck-peterson@users.noreply.github.com>
2019-12-01 21:50:02 -0500

    enhanced error output

--- js/jquery.js
+++ js/jquery.js
@@ -1,46 +1,56 @@
+/* Copyright (CC BY-NC) 2019 by Beck A. Peterson. */
+/* Beck A. Peterson */
+/* beck_peterson@student.uml.edu */
+/* I am affiliated as a student at UMass Lowell in course 91.61 GUI Programming I */
+/* File created on 12/1/19 */
 
 $(document).ready(function () {
 
     function validateInput(input) {
-        return input.match(/^-?\d+$/gi);
+        if (input == "") return "<empty>";
+        return input.replace(/^-?\d+$/gi, "");
     }
 
     $("#topFrom").keyup(function () {
-        if (validateInput($("#topFrom").val())) {
+        var invalidPart = validateInput($("#topFrom").val());
+        if (invalidPart.isEmpty()) {
             $("#topFromWarning").text("");
             $("#topFrom").removeClass("warning");
         } else {
-            $("#topFromWarning").text("Top From input is invalid, please enter a valid number");
+            $("#topFromWarning").text("Top From: '" + invalidPart + "' not recognized, please only enter a valid number");
             $("#topFrom").addClass("warning");
         }
     });
 
     $("#topTo").keyup(function () {
-        if (validateInput($("#topTo").val())) {
+        var invalidPart = validateInput($("#topTo").val());
+        if (invalidPart.isEmpty()) {
             $("#topToWarning").text("");
             $("#topTo").removeClass("warning");
         } else {
-            $("#topToWarning").text("Top To input is invalid, please enter a valid number");
+            $("#topToWarning").text("Top To: '" + invalidPart + "' not recognized, please only enter a valid number");
             $("#topTo").addClass("warning");
         }
     });
 
     $("#sideFrom").keyup(function () {
-        if (validateInput($("#sideFrom").val())) {
+        var invalidPart = validateInput($("#sideFrom").val());
+        if (invalidPart.isEmpty()) {
             $("#sideFromWarning").text("");
             $("#sideFrom").removeClass("warning");
         } else {
-            $("#sideFromWarning").text("Side From input is invalid, please enter a valid number");
+            $("#sideFromWarning").text("Side From: '" + invalidPart + "' not recognized, please only enter a valid number");
             $("#sideFrom").addClass("warning");
         }
     });
 
     $("#sideTo").keyup(function () {
-        if (validateInput($("#sideTo").val())) {
+        var invalidPart = validateInput($("#sideTo").val());
+        if (invalidPart.isEmpty()) {
             $("#sideToWarning").text("");
             $("#sideTo").removeClass("warning");
         } else {
-            $("#sideToWarning").text("Side To input is invalid, please enter a valid number");
+            $("#sideToWarning").text("Side To: '" + invalidPart + "' not recognized, please only enter a valid number");
             $("#sideTo").addClass("warning");
         }
     });
