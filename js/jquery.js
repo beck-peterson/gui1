e6a9ab6f7e76eb3890d6d66717528eec99ae8a35
beck-peterson <56322362+beck-peterson@users.noreply.github.com>
2019-12-01 19:45:07 -0500

    trying keyup for event

--- js/jquery.js
+++ js/jquery.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         return boolValid;
     }
 
-    $("#topFrom").change(function () {
+    $("#topFrom").keyup(function () {
         if (validateInput("Top From entry", $("#topFrom").val())) {
             alert("valid");
         } else {
