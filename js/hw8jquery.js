3d96f15ad528cf57545be49414d829617267fe22
beck-peterson <56322362+beck-peterson@users.noreply.github.com>
2019-12-10 19:31:00 -0500

    Update hw8jquery.js

--- js/hw8jquery.js
+++ js/hw8jquery.js
@@ -1,3 +1,9 @@
+/* Copyright (CC BY-NC) 2019 by Beck A. Peterson. */
+/* Beck A. Peterson */
+/* beck_peterson@student.uml.edu */
+/* I am affiliated as a student at UMass Lowell in course 91.61 GUI Programming I */
+/* File created on 12/8/19 */
+
 var tblValidator = {
     highlightError: function (strVarToTest) {
         $('#' + strVarToTest).css({ "border": "2px solid red" });
@@ -13,17 +19,22 @@ var currentTab = 1;
 function generateMultiplicationTableTab() {
     var topFrom, topTo, sideFrom, sideTo, topInc, sideInc, x, y, tableContents, invalidInput = false;
     currentTab++;
+
+    // Gets the values from the table
     topFrom = parseInt(document.getElementById("topFrom").value);
     topTo = parseInt(document.getElementById("topTo").value);
     sideFrom = parseInt(document.getElementById("sideFrom").value);
     sideTo = parseInt(document.getElementById("sideTo").value);
 
+    // tripple checks theyre valid
     if (isNaN(topFrom) || isNaN(topTo) || isNaN(sideFrom) || isNaN(sideTo)) {
         return;
     }
 
+    // Allows table to be read forward or backward depending on what the user wants the table to do
     topInc = topTo > topFrom ? 1 : -1;
     sideInc = sideTo > sideFrom ? 1 : -1;
+    // Generates the code for the table
     tableContents = "<div id=\"tabs-" + currentTab + "\"><table class=\"multiplicationTable\"><tr><td></td>";
     for (x = topFrom; topTo > topFrom ? x <= topTo : x >= topTo; x += topInc) {
         tableContents += "<td class=\"dark\">" + x + "</td>";
@@ -38,6 +49,7 @@ function generateMultiplicationTableTab() {
         tableContents += "</tr>";
     }
     tableContents += "</table></div>";
+    // Adds the new table in a new tab and selects the new tab
     $("#tabs ul").append("<li><a href=\"#tabs-" + currentTab + "\"></a></li>");
     $("#tabs").append(tableContents);
     $("#tabs").tabs("refresh");
@@ -50,6 +62,7 @@ $(document).ready(function () {
 
     $("#tabs").tabs();
 
+    // Initializes the sliders and sets the parameters and listeners to update the form values
     $("#topFromSlider").slider().width("250px");
     $("#topFromSlider").on("slide", function (event, ui) {
         $("#topFrom").val($("#topFromSlider").slider("option", "value"));
@@ -82,6 +95,7 @@ $(document).ready(function () {
     $("#sideToSlider").slider("option", "max", 51);
     $("#sideToSlider").slider("option", "value", 3);
 
+    // Makes sure not to abliderate slider values if the input isnt valid
     function validateInput(input) {
         if (input === "") return "<empty>";
         return input.replace(/^-?\d+/, ""); // Replaces a valid possitive or negative integer, and returns the rest (the rest would be invalid input)
