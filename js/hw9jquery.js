d351bb09bea29dac890927904bbdb4d75d446952
beck-peterson <56322362+beck-peterson@users.noreply.github.com>
2019-12-17 15:06:15 -0500

    Update hw9jquery.js

--- js/hw9jquery.js
+++ js/hw9jquery.js
@@ -70,7 +70,8 @@ $(document).ready(function () {
 
     var bag = new Bag();
     $(".holder").each(function () {
-        $(this).append("<img src=\"https://beck-peterson.github.io/gui1/gui1/hw9/Scrabble_Tile_" + bag.getTile().char + ".jpg\" width=\"64\" height=\"64\" class=\"draggable\"/>");
+        var tile = bag.getTile();
+        $(this).append("<img src=\"https://beck-peterson.github.io/gui1/gui1/hw9/Scrabble_Tile_" + tile.char + ".jpg\" width=\"64\" height=\"64\" class=\"draggable value" + tile.value + "\"/>");
     });    
 
     $(".draggable").draggable({ snap: ".dropLocation", snapMode: "inner"});
