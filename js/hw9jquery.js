a321c9f8afdb165d1adadda2c8c618f78cb75dbd
beck-peterson <56322362+beck-peterson@users.noreply.github.com>
2019-12-17 17:38:51 -0500

    Update hw9jquery.js

--- js/hw9jquery.js
+++ js/hw9jquery.js
@@ -77,24 +77,92 @@ $(document).ready(function () {
     var currentScore = 0, highScore = 0, currentMultiplier = 1;
     $(".board").each(function () {
         $(this).on("drop", function (event, ui) {
-            currentScore += 1;
+            var tileVal = 0;
+            if (ui.hasClass("value1")) {
+                tileVal = 1;
+            } else if (ui.hasClass("value2")) {
+                tileVal = 2;
+            } else if (ui.hasClass("value3")) {
+                tileVal = 3;
+            } else if (ui.hasClass("value4")) {
+                tileVal = 4;
+            } else if (ui.hasClass("value5")) {
+                tileVal = 5;
+            } else if (ui.hasClass("value8")) {
+                tileVal = 8;
+            } else if (ui.hasClass("value10")) {
+                tileVal = 10;
+            }
+            currentScore += tileVal;
+            $(this).addClass("value" + tileVal);
         });
 
         $(this).on("dropout", function (event, ui) {
             if ($(this).hasClass("draggingFalse")) {
-                currentScore -= 1;
+                var tileVal = 0;
+                if ($(this).hasClass("value1")) {
+                    tileVal = 1;
+                } else if ($(this).hasClass("value2")) {
+                    tileVal = 2;
+                } else if ($(this).hasClass("value3")) {
+                    tileVal = 3;
+                } else if ($(this).hasClass("value4")) {
+                    tileVal = 4;
+                } else if ($(this).hasClass("value5")) {
+                    tileVal = 5;
+                } else if ($(this).hasClass("value8")) {
+                    tileVal = 8;
+                } else if ($(this).hasClass("value10")) {
+                    tileVal = 10;
+                }
+                currentScore -= tileVal;
+                $(this).removeClass("value" + tileVal);
             }
         });
     });
 
     $(".doubleLetter").each(function () {
         $(this).on("drop", function (event, ui) {
-            currentScore += 2;
+            var tileVal = 0;
+            if (ui.hasClass("value1")) {
+                tileVal = 2;
+            } else if (ui.hasClass("value2")) {
+                tileVal = 4;
+            } else if (ui.hasClass("value3")) {
+                tileVal = 6;
+            } else if (ui.hasClass("value4")) {
+                tileVal = 8;
+            } else if (ui.hasClass("value5")) {
+                tileVal = 10;
+            } else if (ui.hasClass("value8")) {
+                tileVal = 16;
+            } else if (ui.hasClass("value10")) {
+                tileVal = 20;
+            }
+            currentScore += tileVal;
+            $(this).addClass("value" + tileVal);
         });
 
         $(this).on("dropout", function (event, ui) {
             if ($(this).hasClass("draggingFalse")) {
-                currentScore -= 2;
+                var tileVal = 0;
+                if ($(this).hasClass("value2")) {
+                    tileVal = 2;
+                } else if ($(this).hasClass("value4")) {
+                    tileVal = 4;
+                } else if ($(this).hasClass("value6")) {
+                    tileVal = 6;
+                } else if ($(this).hasClass("value8")) {
+                    tileVal = 8;
+                } else if ($(this).hasClass("value10")) {
+                    tileVal = 10;
+                } else if ($(this).hasClass("value16")) {
+                    tileVal = 16;
+                } else if ($(this).hasClass("value20")) {
+                    tileVal = 20;
+                }
+                currentScore -= tileVal;
+                $(this).removeClass("value" + tileVal);
             }
         });
     });
